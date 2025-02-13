224bd088ce517dd0894ea7b57d7fa7ff591325d7
Alex <alexo.dejong@outlook.com>
2025-02-13 16:51:06 +1300

    Added account deletion

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Import everthing needed for the project
+# Import everything needed for the project
 from flask import Flask, render_template, url_for, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timezone
@@ -6,125 +6,132 @@ import os
 import pyqrcode
 import shutil
 
-
 app = Flask(__name__, instance_path=os.getcwd())
 
+# Database Configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Main.db'
 db = SQLAlchemy(app)
 
-
-
-qrcode_folder = os.path.join('static', 'qrcodes')  # Sets thefolder for QR codes
+# QR Code Storage Configuration
+qrcode_folder = os.path.join('static', 'qrcodes')
 app.config['UPLOAD_FOLDER'] = qrcode_folder  
-qr_file_path = os.path.join(qrcode_folder, 'QRCODE.png') # Sets the qrcode file path for future use
+qr_file_path = os.path.join(qrcode_folder, 'QRCODE.png')
 
-if os.path.exists(qr_file_path):  # Automatically deletes the QR code if it already exists on server start
+if os.path.exists(qr_file_path):
     os.remove(qr_file_path)
 
 app.secret_key = "secret_key"
 
-class Signin(db.Model):  #  Sets up the signin database as a class
-    # Each one of these columns corresponds to a column in the database and creates a seperate data point with each of these data points in columns
-    id = db.Column(db.Integer, primary_key=True) # Gives each task a unique ID to make sure that tasks with the same name can be used
-    email = db.Column(db.String(100), nullable=False) # Stores the username
-    password = db.Column(db.String(200), nullable=False) # Stores the password
+# User Model
+class Signin(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)  
+    email = db.Column(db.String(100), nullable=False, unique=True)  
+    password = db.Column(db.String(200), nullable=False)  
 
-class Todo(db.Model):  #  Sets up the task manager database as a class
-    # Each one of these columns corresponds to a column in the database and creates a seperate data point with each of these data points in columns
-    id = db.Column(db.Integer, primary_key=True) # Gives each task a unique ID to make sure that tasks with the same name can be used
-    content = db.Column(db.String(200), nullable=False) # Stores the content of the task
-    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc)) # Stores the date created
+# Task Manager Model
+class Todo(db.Model):  
+    id = db.Column(db.Integer, primary_key=True)  
+    content = db.Column(db.String(200), nullable=False)  
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))  
 
     def __repr__(self):
-        return '<Task%r>' % self.id
+        return '<Task %r>' % self.id
 
-
-@app.route("/" , methods=['POST', 'GET'])
+# Login Route
+@app.route("/", methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST': # Checks if the form has been submitted
-        email = request.form["email"] # Gets the email of the user
-        password = request.form["password"] # Gets the password of the user
+    if request.method == 'POST':  
+        email = request.form["email"]
+        password = request.form["password"]
 
-        user = Signin.query.filter_by(email=email).first() # Checks if the user exists
-        if user: # If the user exists
-            if password == user.password: # Checks if the password matches
+        user = Signin.query.filter_by(email=email).first()
+        if user:
+            if password == user.password:  
                 session["signed_in"] = True
-                return redirect('/home') # Redirects to the home page
-            else: # If the password doesn't match
-                return 'Invalid Password' # Returns an error message
-        else: # If the user doesn't exist
-            return 'Invalid Email' # Returns an error message
+                session["user_id"] = user.id  # Store the user ID in session
+                return redirect('/home')  
+            else:
+                return 'Invalid Password'
+        else:
+            return 'Invalid Email'
 
-    return render_template("index.html", signed_in=session.get("signed_in", False)) # Renders the home/main page
+    return render_template("index.html", signed_in=session.get("signed_in", False))
 
+# Home Route
+@app.route("/home")
+def home():
+    if not session.get("signed_in"):
+        return redirect("/")
+    return render_template("home.html")
+
+# Task Manager Route
 @app.route("/taskmanager", methods=['POST', 'GET'])
 def taskmanager():
-    if request.method == 'POST': # Checks if the form has been submitted
-        task_content = request.form["content"] # Gets the content of the task
-        new_task = Todo(content=task_content) # Creates a new task
+    if request.method == 'POST':  
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
 
-        try: # Attempts to add the task
-            db.session.add(new_task) # Adds the task
-            db.session.commit() # Commits the task
-            return redirect('/taskmanager') # Redirects to the task manager page
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/taskmanager')
         except:
-            return 'There was an issue adding your task' # If there is a issue adding the task an error message will appear
-
+            return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all() # Orders the tasks on the page by date created
-        return render_template('taskmanager.html', tasks=tasks) # Renders the task manager page
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('taskmanager.html', tasks=tasks)
 
+# Delete Task Route
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id) # Attempts to get the task for deletion
+    task_to_delete = Todo.query.get_or_404(id)
 
-    try: # Attempts to delete the task
-        db.session.delete(task_to_delete) # Deletes the task
-        db.session.commit() # Commits the deletion
-        return redirect('/taskmanager') # Redirects to the task manager
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/taskmanager')
     except:
-        return 'There was an issue deleting that task' # If there was an issue with deletion an error message will appear
+        return 'There was an issue deleting that task'
 
+# Update Task Route
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
 
     if request.method == 'POST':
         task.content = request.form["content"]
-
         try:
             db.session.commit()
             return redirect('/taskmanager')
         except:
             return "There was an issue updating your task"
-
     else:
         return render_template("update.html", task=task)
-    
+
+# QR Code Generator Route
 @app.route('/QRcode', methods=['POST', 'GET'])
 def qrcode():
-    if not session.get("signed_in"):  # Check if user is signed in
-        return redirect("/")  # Redirect to login page if not signed in
+    if not session.get("signed_in"):
+        return redirect("/")  
     if request.method == 'POST':
-        qrcodestringhtml = request.form['qrstring']
-        qrcodestring = request.form['qrstring'].strip()  # Get and clean the input by removing spaces incase the user pressed space and then entered a string of just spaces
+        qrcodestring = request.form['qrstring'].strip()
         
-        if qrcodestring != '':  # Checks if input is valid and deletes the existing QR code before creating a new one
+        if qrcodestring:
             if os.path.exists(qr_file_path):
                 os.remove(qr_file_path)
 
-            qrcode = pyqrcode.create(qrcodestring)  # Generates the QR code
-            temp_qr_path = 'QRCODE.png' # Saves the QR code as a PNG file
+            qrcode = pyqrcode.create(qrcodestring)
+            temp_qr_path = 'QRCODE.png'
             qrcode.png(temp_qr_path, scale=10)  
 
-            shutil.move(temp_qr_path, qr_file_path) # Moves the QR code to static/qrcodes
-
-            return render_template('qrcode.html', user_qrcode=qr_file_path, qrcodestring=qrcodestringhtml)
+            shutil.move(temp_qr_path, qr_file_path)
+            return render_template('qrcode.html', user_qrcode=qr_file_path, qrcodestring=qrcodestring)
         else:
             return render_template('qrcode.html', qrcodenotvalid="This QR Code is not valid")
 
     return render_template('qrcode.html', user_qrcode=None)
 
+# Signup Route
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
@@ -139,16 +146,32 @@ def signup():
 
     return render_template('signup.html')
 
-@app.route("/home")
-def home():
-    if not session.get("signed_in"):  # Check if user is signed in
-        return redirect("/")  # Redirect to login page if not signed in
-    return render_template("home.html")
-
+# Logout Route
 @app.route("/logout")
 def logout():
-    session.pop("signed_in", None)  # Remove session variable
+    session.pop("signed_in", None)
+    session.pop("user_id", None)
     return redirect("/")
+
+# Delete Account Route
+@app.route("/delete_account")
+def delete_account():
+    if "user_id" not in session:
+        return redirect("/")
+    
+    user_id = session["user_id"]
+    login_detes = Signin.query.get_or_404(user_id)
+    
+    try:
+        db.session.delete(login_detes)
+        db.session.commit()
+        session.pop("user_id", None)
+        session.pop("signed_in", None)
+        return redirect('/')
+    except:
+        return 'There was an issue deleting your account'
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
